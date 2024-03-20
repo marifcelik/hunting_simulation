e9decdead921f9ef5052069d03e5f8536b8d283f
arif <arif-celik56@hotmail.com>
2024-03-21 01:44:04 +0300

    removed unnecessary code

--- animal.go
+++ animal.go
@@ -4,13 +4,8 @@ type AnimalType int
 
 type Animal struct {
 	gender bool // true: female, false: male
-	dead   bool
 }
 
 func NewAnimal(gender bool) *Animal {
-	return &Animal{gender, false}
-}
-
-func (a *Animal) Die() {
-	a.dead = true
+	return &Animal{gender}
 }
